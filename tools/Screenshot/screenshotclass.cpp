d7f00644a1b8ea7216d9cc2a13fc8898cabbbda4
coldbrewski <projectcoldbrew@gmail.com>
2025-12-01 17:09:55 +1030

    updated default path

--- tools/Screenshot/screenshotclass.cpp
+++ tools/Screenshot/screenshotclass.cpp
@@ -1,29 +1,26 @@
 #include "screenshotclass.h"
 
 #include <QDateTime>
+#include <QDir>
 #include <QDebug>
+#include <QFileInfo>
 #include <QImage>
+#include <QStandardPaths>
 #include <QWindow>
-
-/*!
- * \brief DEFAULT_SAVE_PATH
- * \details A default save location for if a
- *  path is not set by the user
- */
-#if defined(Q_OS_WIN)
-    static const QString DEFAULT_SAVE_PATH {"C:/Temp"};
-#elif defined(Q_OS_MAC)
-    static const QString DEFAULT_SAVE_PATH {"/tmp"};
-#else
-    static const QString DEFAULT_SAVE_PATH {"/tmp"};
-#endif
 
 /*!
  * \brief Constructor
  */
 ScreenshotClass::ScreenshotClass(QObject *parent)
-        : QObject{parent}, m_filePath(DEFAULT_SAVE_PATH)
+        : QObject{parent}
 {
+    m_filePath = { QString("%1/%2").arg(
+            QStandardPaths::writableLocation(QStandardPaths::HomeLocation)
+            , "Screenshots/")
+    };
+
+    if (!QFileInfo::exists(m_filePath))
+        QDir().mkpath(m_filePath);
 }
 
 /*!
